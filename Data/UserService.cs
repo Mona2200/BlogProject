255fdc99c01155cfc98eb31b92b202b68ce54c79
Alyona Melekhova <alenamelehova13@gmail.com>
2022-11-25 01:52:11 +0700

    Расширен функционал сервиса пользователей.

--- Data/UserService.cs
+++ Data/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using BlogProject.Models;
+using BlogProject.ViewModels.Response;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -10,6 +11,7 @@ namespace BlogProject.Data
       private readonly BlogDbContext _db = new BlogDbContext();
       private readonly PostService _postService = new PostService();
       private readonly CommentService _commentService = new CommentService();
+      private readonly RoleService _roleService = new RoleService();
 
       public async Task<User[]> GetUsers()
       {
@@ -27,6 +29,53 @@ namespace BlogProject.Data
       {
          var user = await _db.User.Where(u => u.Email == email).FirstOrDefaultAsync();
          return user;
+      }
+
+      private async Task<UserViewModel> FromUserToUserViewModel(User user)
+      {
+         var userViewModel = new UserViewModel();
+         userViewModel.Id = user.Id;
+         userViewModel.FullName = user.FirstName + " " + user.LastName;
+         userViewModel.Sex = user.Sex;
+         userViewModel.Email = user.Email;
+         userViewModel.Roles = await _roleService.GetRoleByUserId(user.Id);
+         userViewModel.Posts = await _postService.GetPostsViewModelByUserId(user.Id);
+
+         return userViewModel;
+      }
+
+      private async Task<UserViewModel[]> FromUsersToUserViewModels(User[] users)
+      {
+         var userViewModels = new UserViewModel[users.Length];
+         int i = 0;
+         foreach (var user in users)
+         {
+            userViewModels[i] = await FromUserToUserViewModel(user);
+            i++;
+         }
+
+         return userViewModels;
+      }
+
+      public async Task<UserViewModel[]> GetUserViewModels()
+      {
+         var users = await _db.User.ToArrayAsync();
+         var userViewModels = await FromUsersToUserViewModels(users);
+         return userViewModels;
+      }
+
+      public async Task<UserViewModel> GetUserViewModelById(Guid id)
+      {
+         var user = await _db.User.Where(u => u.Id == id).FirstOrDefaultAsync();
+         var userViewModel = await FromUserToUserViewModel(user);
+         return userViewModel;
+      }
+
+      public async Task<UserViewModel> GetUserViewModelByEmail(string email)
+      {
+         var user = await _db.User.Where(u => u.Email == email).FirstOrDefaultAsync();
+         var userViewModel = await FromUserToUserViewModel(user);
+         return userViewModel;
       }
 
       public async Task Save(User user)
